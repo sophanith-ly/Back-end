230a890b4c6bde1b1d4c94b96ea6c031c985c6ad
sophanith-ly <sophanithly@gmail.com>
2026-07-03 10:54:42 +0700

    AdminLTE

---
+++ vuejs-app/src/main.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
