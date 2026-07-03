6afb06991cf3eaadd1d7d7bbb9aeeb0b7ff3ee6e
sophanith-ly <sophanithly@gmail.com>
2026-07-03 10:08:33 +0700

    fronend

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/signin', [AuthController::class, 'signin']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/signout', [AuthController::class, 'signout']);
+    Route::get('/verify', [AuthController::class, 'verify']);
+});
